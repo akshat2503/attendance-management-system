39277fb40c9afa634d3dafdbf9df73935b632c70
goldenarrow22 <ronitakki3@gmail.com>
2023-03-02 17:11:37 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,3 +1,4 @@
 <?php 
-$db = mysqli_connect("localhost" , "root" , "root" , "attmgmt") or die("Connectivity Failed");
+echo "Hello";
+// $db = mysqli_connect("localhost" , "root" , "root" , "attmgmt") or die("Connectivity Failed");
 ?>
